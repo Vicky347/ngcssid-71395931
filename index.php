247ff8d84c41f51c71597255c3f4bcd79574721c
Vicky347 <126533896+Vicky347@users.noreply.github.com>
2023-10-20 19:08:41 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Na początku była miłość.</title>
+<title>Na początku była miłość.</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Yuji+Hentaigana+Akari&display=swap" rel="stylesheet">
 <style>
     
-    h1 {
+   h1 {
         display:inline;
-        padding:2pt;
+	padding:2pt;
+	font-size:47pt;
     }
 	body {
   background-image: url('bg.gif');
@@ -19,31 +20,68 @@
   background-position: center;
 	background-origin: content-box;
 	background-size: cover;
-	
 }
+	.g1 {
+		background: rgb(252,252,255);
+background: -moz-linear-gradient(90deg, rgba(252,252,255,0.8037257396813595) 0%, rgba(192,192,219,0.7863207556984642) 35%, rgba(4,4,140,0.4920176477143259) 65%, rgba(7,7,7,0.9856873136578184) 100%);
+background: -webkit-linear-gradient(90deg, rgba(252,252,255,0.8037257396813595) 0%, rgba(192,192,219,0.7863207556984642) 35%, rgba(4,4,140,0.4920176477143259) 65%, rgba(7,7,7,0.9856873136578184) 100%);
+background: linear-gradient(90deg, rgba(252,252,255,0.8037257396813595) 0%, rgba(192,192,219,0.7863207556984642) 35%, rgba(4,4,140,0.4920176477143259) 65%, rgba(7,7,7,0.9856873136578184) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#fcfcff",endColorstr="#070707",GradientType=1);
+	}
 </style>
 </head>
-<body style="color:#effedd;">
-    
-        <h1 style="background:white; color:black; padding-left:5pt; border-bottom:5pt solid black;">Lee</h1><h1 style="background:black; color:white; padding-right:10pt; border-top:5pt solid white;">Moya</h1><h1 style="color:white;"> - droga</h1>
-        <br>
-        <div style="font-family: 'Inconsolata', cursive; font-size:10pt; background:rgba(0,0,0, 0.5);">
-        <br>
+<body style="color:#effedd; margin:0; padding:0; padding-top:5pt;">
+   
+<div style="position:fixed; background:rgba(0,0,0, 0.65); padding-bottom:9pt; padding-top:5pt; padding-left:2pt; width:100%; height:75px;">      
+	<h1 style="margin-left:3pt; background:white; color:black; padding-left:5pt; border-bottom:5pt solid black;">Lee</h1><h1 style="background:black; color:white; padding-right:10pt; border-top:5pt solid white; position:relative; top:4pt;">Moya</h1>
+       
+</div>
+        <div style="font-family: 'Inconsolata', cursive; font-size:10pt; background:rgba(0,0,0, 0.7); padding:2em; margin-top:97px;">
+	<b>-- Droga --</b>
+	<br>
+	<br>
         Istnieją 2 stopnie wtajemniczenia drogi LeeMoya:
-        <br>
-        <ul>
-            <li>biały</li>
-            <li>czarny</li>
+	<br>
+	
+        <ul style="padding-top:0.7em;">
+            <li><b>biały</b></li>
+            <li><b>czarny</b></li>
         </ul>
         <br>
-        Na początku drogi otrzymujemy stopień biały i rozpoczynamy edukację szkoły karate tradycyjnego.
+        Na początku drogi otrzymujemy stopień biały i rozpoczynamy trening w dowolnych kierunkach sztuk walki. Łączymy techniki wielu szkół.
         <br>
-        Filozofia LeeMoya opiera się na 2 elementowej akcji defensywno-ofensywnej: blokujemy cios napastnika i zadajemy cios kończący.
-        <br>
-        Gdy kadet szkoły LeeMoya opracuje własną kombinację ciosów, otrzymuje poziom czarny (DAN).
-        <hr>
-        
-    </div>
+	Filozofia LeeMoya opiera się na 2 elementowej akcji defensywno-ofensywnej (kata): 
+<br>
+<ul style="padding-top:0.7em;">
+<li><i>blokada ciosu napastnika</i></li>
+<li><i>cios kończący</i><br><sub>wraz z jego wyprowadzeniem</sub></li>
+</ul>
+<br>
+Oba elementy mogą być kombinacją wielu technik.
+	<br>
+	<br>
+	<div class="g1" style="padding:2em;">
+	Gdy kadet szkoły LeeMoya opracuje własną kombinację ciosów, otrzymuje poziom czarny (<span style="color:black;">DAN</span>).
+	</div>
+       <br>
+	<br>
+
+	<hr>
+	<br>
+	Szkoła LeeMoya zakłada trening z użyciem narzędzi takich jak nun-chaku, kij bo, bagnet czy miecze a nawet z bronią palną. Zawiera elementy survivalu. Ponadto jest sztuką filozofii, zgodnie z którą nigdy nie dochodzi do walki (zawsze jest 0:0).	
+   <br>
+	<br>
+	<hr>
+	<br>
+	Szkoła została założona w 2023 roku
+	<br>
+	na mocy 24-letniego doświadczenia karate shotokan i kung-fu, jej fundatora
+	<br> 
+	<i>Andrzeja Adamskiego</i> (<b style="color:yellow;">8kyu</b>).
+	<br>
+	<br>
+	
+	 </div>
     <!--
     <iframe src="photo.html?x2" width="320" height="640"></iframe>
     -->
