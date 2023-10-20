13e6dda6a11037e31e132e823200a498795a940f
Vicky347 <126533896+Vicky347@users.noreply.github.com>
2023-10-20 23:11:07 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+
+<table style="width:100%; position:fixed; background:rgba(0,0,0, 0.65); padding-left:2pt; width:100%;">      
+	<tr><td style="width:100%;">
+	<h1 style="margin-left:3pt; background:white; color:black; padding-left:5pt; border-bottom:5pt solid black;">Lee</h1><h1 style="background:black; color:white; padding-right:10pt; border-top:5pt solid white; position:relative; top:4pt;">Moya</h1>
+      </td>
+	<td align=right>
+	 <table style="display:block; border-spacing:8px; height:87px; display:inline; float:right; align:right;">
+	<tr><td style="width:60%;" align=center>
+		<a href="info.php">
+		Informacje
+		</a>
+	</td>
+	<td aligncenter>
+		<a href="login.php">
+		Logowanie/Rejestracja
+		</a>
+	</td>
+	</tr>
+	<tr><td align=center>
+		<a href="cennik.php">
+		Cennik
+		</a>
+	<td align=center>
+		<a href="portfolio/portfolio.html">
+		Portfolio
+	</table>
+</table>
