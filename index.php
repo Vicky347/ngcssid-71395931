1675d2f37206a147b13d5a754c3c5b928fdea6aa
Vicky347 <126533896+Vicky347@users.noreply.github.com>
2023-10-20 12:39:13 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Yuji+Hentaigana+Akari&display=swap" rel="stylesheet">
+<style>
+    
+    h1 {
+        display:inline;
+        padding:2pt;
+    }
+</style>
 </head>
-<body style="background:black;">
-    <div style="font-family: 'Inconsolata', cursive; font-size:17pt; color:white;">
-        <h1>LeeMoya - droga</h1>
+<body style="background:#071777; color:#effedd;">
+    <div style="font-family: 'Inconsolata', cursive; font-size:10pt;">
+        <h1 style="background:white; color:black;">Lee</h1><h1 style="background:black; color:white; padding-right:4pt;">Moya</h1><h1 style="color:white;"> - droga</h1>
+        <br>
+        <br>
         Istnieją 2 stopnie wtajemniczenia drogi LeeMoya:
         <br>
         <ul>
