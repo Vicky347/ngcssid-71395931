f3848938a37dbda979f8b49489a642ec023871cb
Vicky347 <126533896+Vicky347@users.noreply.github.com>
2023-10-24 02:55:32 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<h1 style="margin-left:3pt; background:white; color:black; padding-left:5pt; border-bottom:5pt solid black;">Lee</h1><h1 style="background:black; color:white; padding-right:10pt; border-top:5pt solid white; position:relative; top:4pt;">Moya</h1>
       </td>
 	<td align=right>
-	 <table style="display:block; border-spacing:8px; position:relative; top:-5px; height:87px; display:inline; float:right; align:right;">
+	 <table style="display:block; border-spacing:3px; position:relative; top:-10px; height:87px; display:inline; float:right; align:right;">
 	<tr><td style="width:60%;" align=center>
 		<a href="info.php" id="info">
 		Informacje
