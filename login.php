be160df4cdc7a9d53ba65bd01c56868d0ceb6590
Vicky347 <126533896+Vicky347@users.noreply.github.com>
2023-10-24 02:04:53 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -8,3 +8,17 @@
    <script>
        $('#login').css('color', 'red');
    </script>
+    <div style="font-family: 'Inconsolata', cursive; font-size:10pt; background:rgba(0,0,0, 0.7); padding:2em; margin-top:97px;">
+        <form method="post" action="login.php?dologin">
+            <table>
+                <tr>
+            <td>Twój adres e-mail: <td><input name="user">
+            <tr>
+            <td>Hasło: <td><input type="password" name="pass">
+            <tr>
+            <td colspan=2><input type="submit" value="zaloguj">
+            </table>
+        </form>
+        <br>
+        Nie masz konta? 
+        <a href="reg.php" target="_new">Zarejestruj się</a>
